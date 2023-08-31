c124aac999f0d26657ab8bce50d4a014d81cad7e
Will <doggy.huang@gmail.com>
2023-08-31 20:54:59 +0800

    Add project files.

---
+++ GoogleOAuthDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoogleOAuthDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
